cac18c9b6cd0605b934ef71c0b0d87438acc81ec
elxrojo <elxrojo115@gmail.com>
2026-04-21 10:23:09 -0300

    style: add text and background

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center">
-        Hola
+    <div className="flex flex-1 items-start justify-center">
+      {/* <Image src={"/InProgress.gif"} 
+      fill={true}
+      alt='Guatoncito working'
+      /> */}
+      <h1 className="z-10 text-neutral-50 mt-12 text-6xl text-shadow-[0px_0px_10px_rgba(0,0,0)]">Page in progress</h1>
+
     </div>
   );
 }
