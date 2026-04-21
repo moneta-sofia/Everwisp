4936d7421badec4d244ca5a0b71091bbce88f126
elxrojo <elxrojo115@gmail.com>
2026-04-21 20:57:55 -0300

    style: add button donation

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,58 @@
+import { FaXTwitter } from "react-icons/fa6";
 import Image from "next/image";
+import { tl, tr, bl, br, star } from "@/utils/frame";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 items-start justify-center">
-      {/* <Image src={"/InProgress.gif"} 
-      fill={true}
-      alt='Guatoncito working'
-      /> */}
-      <h1 className="z-10 text-neutral-50 mt-12 text-6xl text-shadow-[0px_0px_10px_rgba(0,0,0)]">Page in progress</h1>
+    <div className="relative flex p-10 flex-col flex-1 items-center justify-between">
+      <Image
+        src={"/InProgress.gif"}
+        fill={true}
+        alt="Guatoncito working"
+        className="-z-10"
+      />
+      <div>
+        <h1 className="text-center text-neutral-50 text-7xl pb-5 text-shadow-[0px_0px_10px_rgba(0,0,0)]">
+          Page in progress
+        </h1>
+        <h2 className="z-10 text-xl font-chivo text-center">
+          Help us to improve our page and community!
+        </h2>
+      </div>
+      <div>
+        <button className="relative font-spirax text-3xl font-medium shadow-goldaura-inset shadow-goldaura btn-magic pointer-events-auto">
+          <Image
+            src={tl}
+            height={75}
+            width={75}
+            alt="frame"
+            className="absolute -top-2 -left-2"
+          />
+          <Image
+            src={tr}
+            height={75}
+            width={75}
+            alt="frame"
+            className="absolute -top-1 -right-2"
+          />
+          <Image
+            src={br}
+            height={75}
+            width={75}
+            alt="frame"
+            className="absolute -bottom-1 -right-2"
+          />
+          <Image
+            src={bl}
+            height={75}
+            width={75}
+            alt="frame"
+            className="absolute -bottom-1 -left-2"
+          />
+          Donate
+        </button>
+      </div>
+
 
     </div>
   );
---
+++ frontend/utils/frame/index.ts
@@ -0,0 +1,5 @@
+export { default as tl } from "./tl.webp";
+export { default as tr } from "./tr.webp";
+export { default as bl } from "./bl_1.webp";
+export { default as br } from "./br.webp";
+export { default as star } from "./star.svg";
