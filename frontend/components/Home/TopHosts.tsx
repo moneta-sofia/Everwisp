76504126fc11b415e6abf111f00aa4626bf79e50
elxrojo <elxrojo115@gmail.com>
2026-05-10 01:17:21 -0300

    feat: base cards

--- frontend/components/Home/TopHosts.tsx
+++ frontend/components/Home/TopHosts.tsx
@@ -3,49 +3,68 @@ import Image from "next/image";
 
 const rankStyles = [
   {
-    card: "w-[300px] h-[420px] scale-110 border-amber-300 shadow-amber-300 order-2 z-20",
-    image: "w-[180px] h-[180px]",
-    badge: "bg-yellow-300 text-white w-14 h-14 text-2xl",
-    title: "text-4xl",
-    description: "text-lg",
-    hours: "text-xl",
-  },
-  {
-    card: "w-[250px] h-[360px] border-slate-400 shadow-[0_0_25px_rgba(226,232,240,0.6)] order-1 z-10",
-    image: "w-[150px] h-[150px]",
-    badge: "bg-slate-200 text-slate-700 w-11 h-11 text-xl",
+    card: "md:w-[190px] w-9/12 md:h-[290px] h-80 scale-110 border-amber-300 shadow-amber-300 order-2 z-20",
+    imageH: 180,
+    imageW: 180,
+    // badge: "bg-yellow-300 text-white w-14 h-14 text-2xl",
     title: "text-3xl",
     description: "text-base",
-    hours: "text-lg",
+    hours: "text-base",
   },
   {
-    card: "w-[250px] h-[360px] border-yellow-700 shadow-[0_0_25px_rgba(251,146,60,0.6)] order-3 z-10",
-    image: "w-[150px] h-[150px]",
-    badge: "bg-orange-500 text-white w-11 h-11 text-xl",
-    title: "text-3xl",
-    description: "text-base",
-    hours: "text-lg",
+    card: "md:w-[185px] w-9/12 md:h-[285px] h-76 border-slate-400 shadow-[0_0_25px_rgba(226,232,240,0.6)] order-1 z-10",
+    imageH: 170,
+    imageW: 170,
+    // badge: "bg-slate-200 text-slate-700 w-11 h-11 text-xl",
+    title: "text-2xl",
+    description: "text-sm",
+    hours: "text-sm",
+  },
+  {
+    card: "md:w-[180px] w-9/12 md:h-[280px] h-76 border-yellow-700 shadow-[0_0_25px_rgba(251,146,60,0.6)] order-3 z-10",
+    imageH: 160,
+    imageW: 160,
+    // badge: "bg-orange-500 text-white w-11 h-11 text-xl",
+    title: "text-xl",
+    description: "text-xs",
+    hours: "text-xs",
   },
 ];
 
-export default function TopHosts(){
-return(
-    <section className="bg-background h-screen py-20 text-full-ilumination w-full">
-        <h1>TOP HOST</h1>
-        <div className="flex justify-center items-center w-full border-amber-300 border-2">
-            {topHostsMock.map((host, i)=> {
-                return(
-                    <div key={i}
-                    className={` ${rankStyles[i].card} flex flex-col justify-center items-center bg-container border-[0.5px] m-5 border-amber-200 btn-magic relative`}
-                    >
-                        <Image src={"/test/porfilePicture.webp"} alt="asd" width={"150"} height={"100"} className="relative -top-10"/>
-                        <h2>{host.userData.name}</h2>
-                        <p>{host.presentation.shortDescription}</p>
-                        <p>{host.stats.hoursStream}</p>
-                    </div>
-                )
-            })}
-        </div>
+export default function TopHosts() {
+  return (
+    <section className="bg-background md:h-screen h-auto py-18 text-full-ilumination w-full">
+      <h1>TOP HOST</h1>
+      <div className="flex justify-center md:flex-row flex-col items-center w-full border-amber-300 border-2">
+        {topHostsMock.map((host, i) => {
+          return (
+            <div
+              key={i}
+              className={` ${rankStyles[i].card} flex flex-col md:my-20 my-10 mx-10 justify-between items-center bg-container border-[0.5px] m-5 border-amber-200 btn-magic relative text-center`}
+            >
+              <Image
+                src={"/test/porfilePicture.webp"}
+                alt="asd"
+                width={rankStyles[i].imageW}
+                height={rankStyles[i].imageH}
+                className="relative -mt-10"
+              />
+              <p className={` ${rankStyles[i].title} font-Laila font-semibold`}>
+                {host.userData.name}
+              </p>
+              <p className={` ${rankStyles[i].description}`}>
+                {host.presentation.shortDescription}
+              </p>
+              <div>
+                <Image src={"/Line.svg"} alt="" width={400} height={10}/>
+                <p className={` ${rankStyles[i].hours} font-chivo font-light pt-3 `}>
+                  {host.stats.hoursStream} HOURS
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
-);
+  );
 }
--- frontend/components/ui/ButtonDecorated.tsx
+++ frontend/components/ui/ButtonDecorated.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { tl, tr, bl, br, star } from "@/utils/frame";
+import { tl, tr, bl, br } from "@/utils/frame";
 import Link from "next/link";
 
 type Props = { 
