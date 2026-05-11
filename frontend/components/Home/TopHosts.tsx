daed544b6ea5e3015cc69c1e42d366012bc1764f
elxrojo <elxrojo115@gmail.com>
2026-05-11 00:02:59 -0300

    style: change h section

--- frontend/components/Home/TopHosts.tsx
+++ frontend/components/Home/TopHosts.tsx
@@ -36,7 +36,7 @@ const rankStyles = [
 
 export default function TopHosts() {
   return (
-    <section className="flex justify-center items-center flex-col bg-background md:h-screen h-auto py-18 text-full-ilumination w-full">
+    <section className="flex justify-center items-center flex-col bg-background md:min-h-screen md:h-auto h-auto pt-18 text-full-ilumination w-full">
       <div className="flex justify-between items-center my-12 w-10/12 container">
         <div className="w-2/3 rotate-180 filter-[drop-shadow(0_0_6px_white)]  md:flex hidden">
           <div className="aguja w-full" />
@@ -48,13 +48,13 @@ export default function TopHosts() {
           <div className="aguja w-full" />
         </div>
       </div>
-      <div className="flex justify-center md:flex-row flex-col items-center w-full">
+      <div className="flex justify-center md:flex-row flex-col items-center w-full h-full">
         {topHostsMock.map((host, i) => {
           return (
             <DecoratedFrame
               key={i}
               color={i === 0 ? "gold" : i === 1 ? "silver" : "bronze"}
-              className={` ${rankStyles[i].card} flex flex-col md:my-20 my-16 mx-10 justify-between items-center bg-container border-[0.5px] m-5 border-amber-200 btn-magic relative text-center`}
+              className={` ${rankStyles[i].card} flex flex-col md:my-16 my-16 mx-10 justify-between items-center bg-container border-[0.5px] m-5 border-amber-200 btn-magic relative text-center`}
             >
               <RankBadge rank={(i + 1) as 1 | 2 | 3} />
               <Image
